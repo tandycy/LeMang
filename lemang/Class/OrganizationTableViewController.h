c460765bcaf259d12210bbb6059d0fef1084e9df
汤 骋原 <tandycy@tangmatoMacBook-Pro.local>
2014-08-19 17:15:35 +0800

    organization view initialize

---
+++ lemang/Class/OrganizationTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  OrganizationTableViewController.h
+//  lemang
+//
+//  Created by 汤 骋原 on 14-8-19.
+//  Copyright (c) 2014年 university media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OrganizationTableViewController : UITableViewController
+
+@end
