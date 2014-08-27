c128943f8e407c36f76a069062bb1d871f724f6e
汤 骋原 <tandycy@tangmatoMacBook-Pro.local>
2014-08-27 16:03:38 +0800

    pull to refresh function added

--- lemang/Class/ActivityViewController.m
+++ lemang/Class/ActivityViewController.m
@@ -9,6 +9,7 @@
 #import "ActivityViewController.h"
 #import "ActivityDetailViewController.h"
 #import "ActivityViewCell.h"
+#import "MJRefresh.h"
 
 #define _AFNETWORKING_ALLOW_INVALID_SSL_CERTIFICATES_
 
@@ -197,6 +198,8 @@ NSString *navTitle;
 {
     NSLog(@"load");
     [super viewDidLoad];
+    
+    [self setupRefresh];
     // Do any additional setup after loading the view.
     //self.searchDisplayController.displaysSearchBarInNavigationBar = YES;
     //activitySearchBar = [[UISearchBar alloc]initWithFrame:CGRectMake(50.0f,0.0f,200.0f,44.0f)];
@@ -301,6 +304,58 @@ NSString *navTitle;
     [self.activityList addSubview:pageControl];
     
 }
+
+- (void)setupRefresh
+{
+    // 1.下拉刷新(进入刷新状态就会调用self的headerRereshing)
+    [self.activityList addHeaderWithTarget:self action:@selector(headerRereshing)];
+#warning 自动刷新(一进入程序就下拉刷新)
+    [self.activityList headerBeginRefreshing];
+    
+    // 2.上拉加载更多(进入刷新状态就会调用self的footerRereshing)
+    [self.activityList addFooterWithTarget:self action:@selector(footerRereshing)];
+    
+    // 设置文字(也可以不设置,默认的文字在MJRefreshConst中修改)
+    self.activityList.headerPullToRefreshText = @"下拉可以刷新了";
+    self.activityList.headerReleaseToRefreshText = @"松开马上刷新了";
+    self.activityList.headerRefreshingText = @"数据刷新中...";
+    
+    self.activityList.footerPullToRefreshText = @"上拉可以加载更多数据了";
+    self.activityList.footerReleaseToRefreshText = @"松开马上加载更多数据了";
+    self.activityList.footerRefreshingText = @"数据加载中...";
+}
+
+#pragma mark 开始进入刷新状态
+- (void)headerRereshing
+{
+    // 1.添加数据
+    [self refreshActivityData];
+    
+    // 2.2秒后刷新表格UI
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        // 刷新表格
+        [self.activityList reloadData];
+        
+        // (最好在刷新表格后调用)调用endRefreshing可以结束刷新状态
+        [self.activityList headerEndRefreshing];
+    });
+}
+
+- (void)footerRereshing
+{
+    // 1.添加数据
+    //[self refreshActivityData];
+    
+    // 2.2秒后刷新表格UI
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        // 刷新表格
+        [self.activityList reloadData];
+        
+        // (最好在刷新表格后调用)调用endRefreshing可以结束刷新状态
+        [self.activityList footerEndRefreshing];
+    });
+}
+
 
 -(void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView1{
     CGPoint offset=scrollView1.contentOffset;
