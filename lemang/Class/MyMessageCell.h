d5ffb11e5e364967dd46d5e482cac8329f18af88
LZ <the_LZ@MacServer.local>
2014-09-23 14:26:16 +0800

    update user message display

--- lemang/Class/MyMessageCell.h
+++ lemang/Class/MyMessageCell.h
@@ -10,16 +10,29 @@
 
 @interface MyMessageCell : UITableViewCell
 {
+    enum MessageCategory
+    {
+        CATEGORY_UNKNOWN,
+        INVITATION_ASSOCIATION,
+		INVITATION_ACTIVITY,
+		INVITATION_FRIEND,
+		ENROLLMENT_ASSOCIATION,
+		ENROLLMENT_ACTIVITY,
+		NOTIFICATION,
+		ALERT,
+		PEER
+    }messCategory;
+    NSMutableDictionary* enumDic;
     //
     NSDictionary* localData;
     id messageTable;
     
     NSNumber* messId;
     
-    NSString* messCategory;
     NSString* messType;
     int messState;  // 0-unread, 1-read, 2-done
     
+    NSString* messTitle;
     NSString* messContent;
 }
 
--- lemang/Class/MyMessageCell.m
+++ lemang/Class/MyMessageCell.m
@@ -34,14 +34,154 @@
     }
 }
 
+- (void)ParseCategory : (NSString*)catg
+{
+    if ([catg isEqualToString:@"INVITATION_ASSOCIATION"])
+        messCategory = INVITATION_ASSOCIATION;
+    else if ([catg isEqualToString:@"INVITATION_ACTIVITY"])
+        messCategory = INVITATION_ACTIVITY;
+    else if ([catg isEqualToString:@"INVITATION_FRIEND"])
+        messCategory = INVITATION_FRIEND;
+    else if ([catg isEqualToString:@"ENROLLMENT_ASSOCIATION"])
+        messCategory = ENROLLMENT_ASSOCIATION;
+    else if ([catg isEqualToString:@"ENROLLMENT_ACTIVITY"])
+        messCategory = ENROLLMENT_ACTIVITY;
+    else if ([catg isEqualToString:@"NOTIFICATION"])
+        messCategory = NOTIFICATION;
+    else if ([catg isEqualToString:@"ALERT"])
+        messCategory = ALERT;
+    else if ([catg isEqualToString:@"PEER"])
+        messCategory = PEER;
+    else
+    {
+        messCategory = CATEGORY_UNKNOWN;
+        NSString* errorMessage = @"Unknown message category: ";
+        errorMessage = [errorMessage stringByAppendingFormat:@"%@", catg];
+        NSLog(@"%@",errorMessage);
+        return;
+    }
+}
+
+- (void)ParseMessageContent
+{
+    NSDictionary* groupData = localData[@"association"];
+    NSDictionary* senderData = localData[@"from"];
+    NSDictionary* activityData = localData[@"activity"];
+    
+    switch (messCategory) {
+        case INVITATION_ASSOCIATION:
+        {
+            if (![groupData isKindOfClass:[NSDictionary class]])
+            {
+                NSLog(@"Missing group data for INVITATION_ASSOCIATION message.");
+                return;
+            }
+            NSString* nameStr = [UserManager filtStr: groupData[@"name"]];
+            messTitle = @"收到组织邀请";
+            messContent = [NSString stringWithFormat:@"收到组织<%@>的邀请\n是否加入该组织？",nameStr];
+        }
+            break;
+        case INVITATION_ACTIVITY:
+        {
+            if (![activityData isKindOfClass:[NSDictionary class]])
+            {
+                NSLog(@"Missing activity data for INVITATION_ACTIVITY message.");
+                return;
+            }
+            NSString* nameStr = [UserManager filtStr: activityData[@"title"]];
+            messTitle = @"收到活动邀请";
+            messContent = [NSString stringWithFormat:@"收到活动<%@>的邀请\n是否参加该活动？",nameStr];
+        }
+            break;
+        case INVITATION_FRIEND:
+        {
+            if (![senderData isKindOfClass:[NSDictionary class]])
+            {
+                NSLog(@"Missing sender data for INVITATION_FRIEND message.");
+                return;
+            }
+            NSString* nameStr = [UserManager filtStr: senderData[@"name"]];
+            messTitle = @"收到好友邀请";
+            messContent = [NSString stringWithFormat:@"收到<%@>的好友邀请\n是否同意？",nameStr];
+        }
+            break;
+        case ENROLLMENT_ASSOCIATION:
+        {
+            if (![groupData isKindOfClass:[NSDictionary class]])
+            {
+                NSLog(@"Missing group data for ENROLLMENT_ASSOCIATION message.");
+                return;
+            }
+            if (![senderData isKindOfClass:[NSDictionary class]])
+            {
+                NSLog(@"Missing sender data for ENROLLMENT_ASSOCIATION message.");
+                return;
+            }
+            NSString* groupName = [UserManager filtStr: groupData[@"name"]];
+            NSString* senderName = [UserManager filtStr: senderData[@"name"]];
+            messTitle = @"收到组织加入请求";
+            messContent = [NSString stringWithFormat:@"<%@>申请加入组织<%@>\n是否同意？",senderName,groupName];
+        }
+            break;
+        case ENROLLMENT_ACTIVITY:
+        {
+            if (![activityData isKindOfClass:[NSDictionary class]])
+            {
+                NSLog(@"Missing activity data for ENROLLMENT_ACTIVITY message.");
+                return;
+            }
+            if (![senderData isKindOfClass:[NSDictionary class]])
+            {
+                NSLog(@"Missing sender data for ENROLLMENT_ACTIVITY message.");
+                return;
+            }
+            NSString* activityName = [UserManager filtStr: activityData[@"title"]];
+            NSString* senderName = [UserManager filtStr: senderData[@"name"]];
+            messTitle = @"收到活动参加请求";
+            messContent = [NSString stringWithFormat:@"<%@>申请参加活动<%@>\n是否同意？",senderName,activityName];
+        }
+            break;
+        case NOTIFICATION:
+        {
+             messTitle = @"收到提醒";
+        }
+            break;
+        case ALERT:
+        {
+             messTitle = @"收到警告";
+        }
+            break;
+        case PEER:
+        {
+             messTitle = @"收到关注";
+        }
+            break;
+        default:
+        {
+            //
+        }
+            break;
+    }
+
+}
+
 - (void)SetMessageData:(NSDictionary *)data owner:(id)owner
 {
+   //key: id category association from content status to activity title
+     
     localData = data;
     messageTable = owner;
+    messCategory = CATEGORY_UNKNOWN;
+    
+    messTitle = @"";
+    messContent = @"";
     
     messType = localData[@"type"];
     messId = localData[@"id"];
-    messCategory = localData[@"category"];
+    NSString* messCtg = localData[@"category"];
+    
+    [self ParseCategory:messCtg];
+    [self ParseMessageContent];
     
     NSString* state = localData[@"status"];
     if ([state isEqualToString:@"UNREAD"])
@@ -65,15 +205,7 @@
         messState = -1;
     }
     
-    _messageTitle.text = localData[@"title"];
-    messContent = [UserManager filtStr : localData[@"content"] : @""];
-    /*
-     association
-     from
-     content
-     to
-     activity
-     */
+    _messageTitle.text = messTitle;
 }
 
 - (NSNumber*)GetMessageId
@@ -96,7 +228,7 @@
         [readRequest startSynchronous];
     }
     
-    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:_messageTitle.text message:messContent delegate:self cancelButtonTitle:nil otherButtonTitles:@"ok", nil];
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:messTitle message:messContent delegate:self cancelButtonTitle:nil otherButtonTitles:@"ok", nil];
     [alertView show];
     return;
 }
