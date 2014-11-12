cfb63fe002d0ae849f8158914057afa4f5032f70
汤 骋原 <tandycy@tangmatoMacBook-Pro.local>
2014-11-12 12:04:27 +0800

    ui optimized

--- lemang/Class/HomeTableViewController.m
+++ lemang/Class/HomeTableViewController.m
@@ -60,7 +60,7 @@
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-    
+    [self InitFooterHeader];
     [self RefreshNewList];
 }
 
